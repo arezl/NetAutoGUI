2b8196bda2c9dbf030f0d67e8ab03c95e4490e67
card4j8 <425199688@qq.com>
2024-11-17 15:27:33 +0800

    add some test code

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using NetAutoGUI;
 
 class Program
 {
@@ -15,6 +16,23 @@ class Program
 
             foreach (string file in files)
             {
+
+
+
+                try
+                {
+                    ClickImage(1);
+                    ClickImage(2);
+                    // GUI.Keyboard.Press(GUI.Keyboard.Keys.Control, GUI.Keyboard.Keys.C);
+                    GUI.Keyboard.HotKey(VirtualKeyCode.CONTROL, VirtualKeyCode.VK_V);
+                        GUI.Keyboard.Press(VirtualKeyCode.RETURN);
+                         
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"An error occurred: {ex.Message}");
+                }
+
                 Console.WriteLine(file);
             }
         }
@@ -22,5 +40,12 @@ class Program
         {
             Console.WriteLine("Directory does not exist.");
         }
+    }
+
+    private static void ClickImage(int i)
+    {
+        var rect = GUI.Screenshot.LocateAllOnScreen($"test{i}.png").First();
+        GUI.Mouse.Click(rect.X, rect.Y);
+        Thread.Sleep(2000);
     }
 }
